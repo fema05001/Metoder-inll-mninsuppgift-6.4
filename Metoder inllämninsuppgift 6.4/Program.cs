62f73d225407eaf7375eb2692682b3f0ad1ddd2f
Elev <Elev@TEB125102>
2023-03-03 15:02:16 +0100

    Add project files.

---
+++ Metoder inllämninsuppgift 6.4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace inllämningsupgift
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett tal");
+            Console.WriteLine(" ");
+            string a = Console.ReadLine();
+            double b =double.Parse(a);
+            Console.WriteLine("vilken potens vill du se talet i?");
+            Console.WriteLine(" ");
+
+            string c =Console.ReadLine();
+            double d =double.Parse(c);
+            Console.WriteLine("potensen av " + a + " i " + c + " blir " + Potensberäkning(b, d));
+        }
+        static double Potensberäkning(double tal1, double tal2)
+        {
+            
+            {
+                double tal3;
+                for (double i =0; tal2 < i; i++)
+                {
+                    tal3= tal1 * tal1 ;
+                }
+                return tal3;
+            }
+        }
+    }
+}
